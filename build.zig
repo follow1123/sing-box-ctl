9163605dda3b5308490402fc0a430a859a01115f
yf <--->
2025-07-20 16:40:53 +0800

    ci: 添加构建多平台的发布版本

--- build.zig
+++ build.zig
@@ -20,12 +20,17 @@ const Params = struct {
     }
 };
 
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
     var params = Params.init(b);
 
-    const go_build = try goBuildStep(b, target, &params);
+    const go_build = try goBuildStep(b, target, &params, null);
     b.getInstallStep().dependOn(&go_build.step);
 
     const go_run = try goRunStep(b, &params);
@@ -33,6 +38,15 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the Go application");
     run_step.dependOn(&go_run.step);
+
+    const targets_dir = "targets";
+
+    const releases_step = b.step("releases", "Build all target releases");
+
+    for (targets) |t| {
+        const go_release_build = try goBuildStep(b, b.resolveTargetQuery(t), &params, targets_dir);
+        releases_step.dependOn(&go_release_build.step);
+    }
 }
 
 fn goRunStep(b: *std.Build, params: *Params) !*std.Build.Step.Run {
@@ -42,13 +56,25 @@ fn goRunStep(b: *std.Build, params: *Params) !*std.Build.Step.Run {
     return go_run;
 }
 
-fn goBuildStep(b: *std.Build, target: std.Build.ResolvedTarget, params: *Params) !*std.Build.Step.Run {
-    const mod_name = try getGoModuleName(b.allocator);
-    const go_build = b.addSystemCommand(&.{ "go", "build", "-o", params.app_full_path });
+fn goBuildStep(b: *std.Build, target: std.Build.ResolvedTarget, params: *Params, release_targets_dir: ?[]const u8) !*std.Build.Step.Run {
+    const root_file_path = try b.build_root.join(b.allocator, &.{"go.mod"});
+    const mod_name = try getGoModuleName(b.allocator, root_file_path);
+
+    var prod = params.prod;
+    var app_full_path: []const u8 = undefined;
+    if (release_targets_dir) |targets_dir| {
+        prod = true;
+        const target_path = try target.query.zigTriple(b.allocator);
+        app_full_path = b.pathJoin(&.{ b.install_prefix, targets_dir, target_path, APP_NAME });
+    } else {
+        app_full_path = params.app_full_path;
+    }
+
+    const go_build = b.addSystemCommand(&.{ "go", "build", "-o", app_full_path });
 
     var ldflags = std.ArrayList([]const u8).init(b.allocator);
-    try ldflags.append(b.fmt("-X {s}/logger.Production={s}", .{ mod_name, if (params.prod) "true" else "false" }));
-    if (!params.prod) {
+    try ldflags.append(b.fmt("-X {s}/logger.Production={s}", .{ mod_name, if (prod) "true" else "false" }));
+    if (!prod) {
         try ldflags.append(b.fmt("-X {s}/config.ConfigHome={s}", .{ mod_name, params.dev_config_home }));
     }
 
@@ -60,8 +86,8 @@ fn goBuildStep(b: *std.Build, target: std.Build.ResolvedTarget, params: *Params)
     return go_build;
 }
 
-fn getGoModuleName(alloc: std.mem.Allocator) ![]const u8 {
-    const mod_file = try std.fs.cwd().openFile("go.mod", .{});
+fn getGoModuleName(alloc: std.mem.Allocator, mod_file_path: []const u8) ![]const u8 {
+    const mod_file = try std.fs.openFileAbsolute(mod_file_path, .{});
     defer mod_file.close();
     var reader = std.io.bufferedReader(mod_file.reader());
     const r = reader.reader();
