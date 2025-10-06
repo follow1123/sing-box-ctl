a3514a66928ad984cc52ffa9cbad98d9f1caa163
yf <--->
2025-10-06 19:37:02 +0800

    feat: 添加log子命令

---
+++ cmd/log.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/follow1123/sing-box-ctl/config"
+	"github.com/follow1123/sing-box-ctl/jsonhandler"
+	"github.com/follow1123/sing-box-ctl/platform"
+	"github.com/spf13/cobra"
+)
+
+var logCmd = &cobra.Command{
+	Use:   "log",
+	Short: "print log",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+		// 初始化配置
+		conf, err := config.Default()
+		if err != nil {
+			return err
+		}
+		jh, err := jsonhandler.FromFile(conf.SingBoxConfigPath())
+		if err != nil {
+			return err
+		}
+		logName, exists := jh.GetString("log.output")
+		if !exists {
+			return fmt.Errorf("no log file")
+		}
+		logFile := filepath.Join(conf.SingBoxWorkingDir(), logName)
+		fmt.Printf("sing-box log file: %s\n\n", logFile)
+		if err := platform.LogFile(logFile); err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(logCmd)
+}
